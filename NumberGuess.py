77218a97226835be8ab409bce759762516bd0346
breadman22 <42971506+ThatGuyBreadman1567@users.noreply.github.com>
2018-11-13 11:14:36 -0500

    Update NumberGuess.py

--- NumberGuess.py
+++ NumberGuess.py
@@ -1,4 +1,11 @@
 #Benjamin Readman
 #NumberGuess
 import random
-random.randrange(1,20)
+num=random.randrange(1,20)
+numg=int(input("Please try to guess the number I'm thinking of."))
+if numg == num:
+  print("Good job, you guessed, ",num,". That is correct.", sep=(""))
+elif numg >= 1 and numg <= 20 and numg != num:
+  print("Sorry, that is not the correct number. The number I was thinking of was, ",num,".",sep=(""))
+elif numg > 20 or numg < 1:
+  print("That is not exceptable, goodbye.")
